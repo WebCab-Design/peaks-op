75976c3f50fc96b8e8a8a21ed48a32098fd43225
AlexanderElias <alex.steven.elias@gmail.com>
2016-10-28 17:02:12 -0700

    implemented form

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,25 +3,22 @@
 	'use strict';
 
 	At.submit({
-		query: '.form-contact',
+		query: '.contact-form',
 		prepare: function (data) {
-			console.log(data);
-			data.foo = 'bar';
+			delete data._format;
 		},
 		complete: function (error, success) {
-			var response = document.querySelector('.form-response');
+			var response = document.querySelector('.contact-response');
 
 			if (error) {
 				console.log(error);
 				response.style.color = 'red';
-				response.innerText = 'Error';
-			}
-			else {
-				response.style.color = 'green';
-				response.innerText = 'Form Is Sent';
+				response.innerText = 'Error Please See Console';
+			} else {
+				response.innerText = 'Contact Form Sent';
 			}
 
 		}
 	});
-	
+
 }());
