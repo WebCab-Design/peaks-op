4632852dd2f9a33767a03119e24728c60f305ee7
Alexnader Elias <alex.steven.elias@gmail.com>
2016-11-16 15:33:56 -0700

    fixed insurance form, added js submit

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -20,4 +20,23 @@
 		}
 	});
 
+	At.submit({
+		query: '.insurance-form',
+		prepare: function (data) {
+			data._cc = 'bburns@peaksrecovery.com';
+		},
+		complete: function (error, success) {
+			var response = document.querySelector('.insurance-response');
+
+			if (error) {
+				console.log(error);
+				response.style.color = 'red';
+				response.innerText = 'Error Please See Console';
+			} else {
+				response.innerText = 'Verify Insurance Form Sent';
+			}
+
+		}
+	});
+
 });
