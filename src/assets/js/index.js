a9d4a8bdc05cdce9aa77bd68024efee1b4ce60d7
Alexander Elias <alex.steven.elias@gmail.com>
2016-11-22 18:09:53 -0700

    fixed form submission issue

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,52 +1,38 @@
-(function() {
-	'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+	var form = document.querySelector('.form');
 
-	At.submit({
-		query: '.contact-form',
-		prepare: function (data) {
-			data._cc = 'bburns@peaksrecovery.com';
-		},
-		complete: function (error, success) {
-			var response = document.querySelector('.contact-response');
+	if (form) {
+		Astatine.submit({
+			query: form,
+			mimeType: 'json',
+			prepare: function (data) {
+				data._cc = 'bburns@peaksrecovery.com';
+			},
+			complete: function (error, success) {
+				var response = document.querySelector('.response');
 
-			if (error) {
-				console.log(error);
-				response.style.color = 'red';
-				response.innerText = 'Error Please See Console';
-			} else {
-				response.innerText = 'Contact Form Sent';
+				if (error) {
+					console.log(error);
+					response.style.color = 'red';
+					response.innerText = 'Error Please See Console';
+				} else {
+					form.style.display = 'none';
+					response.style.color = 'green';
+					response.innerText = 'Form Submission Sent';
+				}
 			}
-
-		}
-	});
-
-	At.submit({
-		query: '.insurance-form',
-		prepare: function (data) {
-			data._cc = 'bburns@peaksrecovery.com';
-		},
-		complete: function (error, success) {
-			var response = document.querySelector('.insurance-response');
-
-			if (error) {
-				console.log(error);
-				response.style.color = 'red';
-				response.innerText = 'Error Please See Console';
-			} else {
-				response.innerText = 'Verify Insurance Form Sent';
-			}
-
-		}
-	});
+		});
+	}
 
 	// var bioButton = document.querySelector('.bio');
 	// var bioContent = document.querySelector('.bio-more');
 	// var closeButton = document.querySelector('.close');
-	// 
+	//
 	// bioButton.addEventListener('click', function(){
 	// 	bioContent.classList.toggle('active');
 	// });
 	// closeButton.addEventListener('click', function(){
 	// 	closeButton.classList.toggle('active')
 	// });
+
 });
