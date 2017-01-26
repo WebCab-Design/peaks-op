f115caf785e2484ec6f17571a1dfd66acab97b8d
Alexander Elias <alex.steven.elias@gmail.com>
2017-01-26 02:15:21 -0700

    fixed style, matched peaks recovery center

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,24 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var form = document.querySelector('.form');
 
+	var form = document.querySelector('.form');
 	if (form) {
 		Astatine.submit({
 			query: form,
-			mimeType: 'json',
-			prepare: function (data) {
-				data._cc = 'bburns@peaksrecovery.com';
+			method: 'post',
+			responseType: 'json',
+			action: 'https://www.enformed.io/zpn17s0',
+			prepare: function (data, resolve) {
+				data['*default_email'] = 'bburns@peaksrecovery.com';
+				return resolve(data);
 			},
 			complete: function (error, success) {
 				var response = document.querySelector('.response');
-
 				if (error) {
 					console.log(error);
 					response.style.color = 'red';
-					response.innerText = 'Error Please See Console';
+					response.innerText = 'Error Plese See Console';
 				} else {
-					form.style.display = 'none';
 					response.style.color = 'green';
-					response.innerText = 'Form Submission Sent';
+					response.innerText = 'Form Is Submitted';
 				}
 			}
 		});
